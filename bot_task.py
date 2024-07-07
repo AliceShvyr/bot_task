5e816d8a1dee0f10654a1a86b36e23b2a549b82e
AliceShvyr <limiko76.9@gmail.com>
2024-07-07 21:24:33 +0300

    clear

--- bot_task.py
+++ bot_task.py
@@ -65,7 +65,7 @@ async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id, 
-        text="clearped")
+        text="The list has been successfully cleared")
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
